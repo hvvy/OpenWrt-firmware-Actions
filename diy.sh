cfeefc164e0e44899b5eede9f1d4af416553fe68
hvvy <56221924+hvvy@users.noreply.github.com>
2020-03-11 10:44:27 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,7 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+git clone https://github.com/Leo-Jo/luci-app-koolproxyR.git package/luci-app-koolproxyR
+git clone https://github.com/hvvy/luci-app-serverchan.git package/luci-app-serverchan
+./scripts/feeds update -a
+./scripts/feeds install -a
