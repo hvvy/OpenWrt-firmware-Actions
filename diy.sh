14818a774bbe0379518b767dcc769a58d10fd562
hvvy <56221924+hvvy@users.noreply.github.com>
2020-03-12 02:17:37 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -10,6 +10,6 @@
 git clone https://github.com/Leo-Jo/luci-app-koolproxyR.git package/luci-app-koolproxyR
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
-git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+git clone https://github.com/vernesong/OpenClash package/luci-app-openclash
 ./scripts/feeds update -a
 ./scripts/feeds install -a
